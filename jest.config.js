793bbdefd88f0525cbb73d0082ade435eb3fdc22
Juan Jose Ramirez <artjjrn@gmail.com>
2023-12-11 13:09:13 -0300

    Configure Jest

--- jest.config.js
+++ jest.config.js
@@ -2,18 +2,32 @@
 module.exports = {
   preset: 'ts-jest',
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '\\.ts$': 'ts-jest',
   },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    "^@constants/(.*)$": '<rootDir>/src/constants/$1',
+    "^@lib/(.*)$": '<rootDir>/src/lib/$1',
+    "^@nostr/(.*)$": '<rootDir>/src/nostr/$1',
+    "^@rest/(.*)$": '<rootDir>/src/rest/$1',
+    "^@services/(.*)$": '<rootDir>/src/services/$1',
+    "^@type/(.*)$": '<rootDir>/src/type/$1',
   },
   testMatch: [
-    '<rootDir>/**/(*.)test.(js|ts)',
-    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+    '<rootDir>/tests/**/*.test.ts',
   ],
   testEnvironment: 'node',
   clearMocks: true,
   collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+  ],
   coverageDirectory: '.coverage',
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  coverageProvider: 'v8',
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+  ],
+  setupFiles: [
+    'dotenv/config',
+  ],
 };
